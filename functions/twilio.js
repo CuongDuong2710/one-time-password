55acd68f9d3b4e98c8448880688a92f04d74a39c
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-27 16:37:43 +0700

    install twilio library and twilio credentials

---
+++ functions/twilio.js
@@ -0,0 +1,6 @@
+const twilio = require('twilio')
+
+const accountSid = 'ACace70b6b50f2ccc8e98ed4e5b11072b3'
+const authToken = '419d234bf232a644eabcdffd435492c3'
+
+module.exports = new twilio.Twilio(accountSid, authToken)
