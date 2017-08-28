f953c29f58c2f5eef6f7c7cca14b8849365b94e9
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-28 14:29:51 +0700

    save code into user model database

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -8,14 +8,25 @@ module.exports = function(req, res) {
 
     const phone = String(req.body.phone).replace(/[^\d]/g, '')
 
+    // Get user model by phone
     admin.auth().getUser(phone)
       .then(userRecord => {
+
+        // Generate code
         const code = Math.floor((Math.random() * 8999 + 1000))
 
+        // Sending texts to user, asynchonorous action use callback 'err' object
         twilio.messages.create({
             body: 'Your code is ' + code,
             to: phone,
             from: '+12404396725'
+        }, (err) => {
+            if (err) { return res.status(422).send(err) }
+
+            admin.database().ref('users/' + phone)
+              .update({ code: code, codeValid: true }, () => {
+                  res.send({ success: true })
+              })
         })
       })
       .catch((err) => {
--- functions/twilio.js
+++ functions/twilio.js
@@ -3,4 +3,5 @@ const twilio = require('twilio')
 const accountSid = 'ACace70b6b50f2ccc8e98ed4e5b11072b3'
 const authToken = '419d234bf232a644eabcdffd435492c3'
 
+// Twilio Credentials
 module.exports = new twilio.Twilio(accountSid, authToken)
