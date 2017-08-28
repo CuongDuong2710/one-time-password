a783ed087ac503b4192afbd60023c70bccf73f2f
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-28 16:10:13 +0700

    creating JWT and ref off listen value changes

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -6,13 +6,16 @@ module.exports = function(req, res) {
     }
 
     const phone = String(req.body.phone).replace(/[^\d]/g, '')
-    const code = parseInt(code)
+    const code = parseInt(req.body.code)
 
     admin.auth().getUser(phone)
       .then(() => {
         // fetch amound of some 'value', call this callback function 'snapshot' of the data was retrieved
         const ref = admin.database().ref('users/' + phone)
         ref.on('value', snapshot => {
+
+          // 'Value' don't attempt to listen for any more value changes
+          ref.off()
 
           // Get user
           const user = snapshot.val()
@@ -24,7 +27,12 @@ module.exports = function(req, res) {
             return res.status(422).send({ error: 'Code not valid' })
           }
 
+          // code is used one time, so invalid it
           ref.update({ codeValid: false })
+
+          // create JWT
+          admin.auth().createCustomToken(phone)
+            .then(token => res.send({ token: token }))
         })
       })
       .catch((err) => res.status(422).send({ error: err }))
